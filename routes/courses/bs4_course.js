fc02d6bdab44897b9a68ad759029b7e7dd8ae946
Umair Hussain <umairhussaindhillon@gmail.com>
2020-08-14 16:10:24 +0500

    improved courses fetch route

--- routes/courses/bs4_course.js
+++ routes/courses/bs4_course.js
@@ -6,11 +6,25 @@ var router = express.Router();
 router.post('/bs4courses',(req,res)=>{
     console.log(req.body)
     semester=req.body.semester
+    connection.execute('SELECT * FROM `courses` WHERE `semester`=?', [semester])
+    .then(([rows]) => {
+        if(rows.length === 0){
+          console.log(rows.length)
+         //   return Promise.reject('This E-mail already in use!');
+         res.send(`error occured! `);
+
+        }
+        res.send(rows);
+    }).catch(err => {
+        // THROW INSERTING USER ERROR'S
+        if (err) throw err;
+    });
+  
+ /*
     connection.query("select * from courses where semester="+semester,(err,array,feilds)=>{
         if (err) throw err
         else
         res.send(array);
-    });
+    });*/
 });
-
 module.exports = router;
