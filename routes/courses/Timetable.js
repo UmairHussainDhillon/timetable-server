a0bf2093da97fe94eca451426b77ca747e65a26d
Umair Hussain <umairhussaindhillon@gmail.com>
2020-08-30 14:30:22 +0500

    Added Auto Classroom in Timetable Route

--- routes/courses/Timetable.js
+++ routes/courses/Timetable.js
@@ -7,46 +7,46 @@ const { Router } = require("express");
 var router = express.Router();
 
 router.post("/timetable", (req, res) => {
-   console.log(req.body);
+  console.log(req.body);
 
- var  instructor_id ,
- Course_id = req.body.Course_id;
-    day_id = req.body.day_id,
-    slot_id = req.body.slot_id,
-    slot = req.body.slot_id,
-    classroom_id = req.body.classroom_id;
+  var instructor_id;
+ // var classroom_id;
+
+    Course_id = req.body.Course_id;
+  (day_id = req.body.day_id),
+    (slot_id = req.body.slot_id),
+    (slot = req.body.slot_id);
+   classroom_id = 1;
   //  course_id = req.body.course_id;
   let errors = [];
-  let Results ={};
-  var StrongConstraint=false;
-  var WeakConstraint=false;
+  let Results = {};
+  var StrongConstraint = false;
+  var WeakConstraint = false;
 
   var timetable = [];
-  //To get assisgned teacher of Course 
-  dbConnection.execute("SELECT * FROM `courses` WHERE `course_id`=?", [
-    Course_id,
-  ]).then(([rows]) => {
-    console.log(rows.length)
-    if(rows[0].instructor_id==null){
-      console.log("No Instructor")
-      errors.push("Please Assign a Instructor First")
-    }
-    instructor_id = rows[0].instructor_id;
-
-  });
+  //To get assisgned teacher of Course
+  dbConnection
+    .execute("SELECT * FROM `courses` WHERE `course_id`=?", [Course_id])
+    .then(([rows]) => {
+      console.log(rows.length);
+      if (rows[0].instructor_id == null) {
+        console.log("No Instructor");
+        errors.push("Please Assign a Instructor First");
+      }
+      instructor_id = rows[0].instructor_id;
+    });
 
   dbConnection
     .execute("SELECT * FROM `timetable` ")
     .then(([rows]) => {
       timetable = rows;
 
-              //Constraints  Bussiness Logic
+      //Constraints  Bussiness Logic
 
       // Friday Prayer Time
-      if(day_id == 5 && slot_id ==3){
+      if (day_id == 5 && slot_id == 3) {
         errors.push("Friday Prayer Time");
-        StrongConstraint=true;
-
+        StrongConstraint = true;
       }
       // for same Instructor
       for (let i = 0; i < timetable.length; i++) {
@@ -56,7 +56,7 @@ router.post("/timetable", (req, res) => {
           timetable[i].day_id == day_id
         ) {
           errors.push("Instructor Can't teach two Classes at Same Time");
-          StrongConstraint=true;
+          StrongConstraint = true;
           //     console.log(timetable[i].timetable_id);
           //  status= false;
           break;
@@ -70,32 +70,39 @@ router.post("/timetable", (req, res) => {
           timetable[i].day_id == day_id
         ) {
           errors.push("Two Classes of Same Course cant be at same time");
-          StrongConstraint=true;
+          StrongConstraint = true;
 
           //     console.log(timetable[i].timetable_id);
           //  status= false;
           break;
         }
       }
-      // for same Classroom at same Time
+      // for  Classroom ID
       for (let i = 0; i < timetable.length; i++) {
-        if (
-          timetable[i].classroom_id == classroom_id &&
-          timetable[i].slot_id == slot_id &&
-          timetable[i].day_id == day_id
-        ) {
-          errors.push("Classroom Can't be used for Two Classes at Same Time");
-          StrongConstraint=true;
-
-          break;
+        console.log(classroom_id)
+       if(classroom_id === 5) {
+       // No Classroom is Free
+         errors.push("No Classroom is Free");
+         WeakConstraint=true;
+       }
+       else{if ( timetable[i].day_id == day_id) {
+        // To go to Particular Day in Table:
+          if(timetable[i].slot_id==slot_id){
+         // For Particular Slot in Table: ")
+           if(timetable[i].classroom_id===classroom_id)
+           //If First Classroom is Not free Check Next One)
+            classroom_id++;
+          }
         }
-      }
+      }}
+
       //
     })
     .then((user) => {
-      // INSERTING USER INTO DATABASE
+      // INSERTING Table Row INTO DATABASE
+      console.log("Errors Length: "+errors.length)
       if (errors.length == 0) {
-        // console.log(handleconstraint())
+        console.log(Course_id,instructor_id,slot_id,day_id,classroom_id)
         dbConnection
           .execute(
             "INSERT INTO `timetable`(`course_id`, `instructor_id`, `slot_id`,`day_id`,`classroom_id`) VALUES(?,?,?,?,?)",
@@ -106,19 +113,18 @@ router.post("/timetable", (req, res) => {
             res.status(200).send(`Added in Table Successfully`);
           })
           .catch((err) => {
-            // THROW INSERTING USER ERROR'S
+            // THROW INSERTING  ERROR'S
             if (err) {
               res.send(err);
             }
           });
       } else {
-        Results.errors=errors;
-        Results.StrongConstraint=StrongConstraint;
-        Results.WeakConstraint=WeakConstraint;
-        console.log(Results)
+        Results.errors = errors;
+        Results.StrongConstraint = StrongConstraint;
+        Results.WeakConstraint = WeakConstraint;
+        console.log(Results);
         res.send(Results);
       }
-  
     });
 });
 
